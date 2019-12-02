6da63bb222f7ecc84049df57208b15298d151b6a
felipe <felipe@vemlavaralouca.com.br>
2019-12-02 14:38:30 -0300

    d

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <MemoryCard></MemoryCard> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import MemoryCard from '@/components/MemoryCard.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    // MemoryCard
   }
 }
 </script>
